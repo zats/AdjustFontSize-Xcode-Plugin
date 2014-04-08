89b8261c99da92b7937758cfbdfef367b16c752e
Sasha Zats <sasha@zats.io>
2014-04-08 12:04:10 +0300

    Initial Commit

---
+++ AdjustFontSize/ZTSAdjustFontSize.h
@@ -0,0 +1,13 @@
+//
+//  ZTSAdjustFontSize.h
+//  ZTSAdjustFontSize
+//
+//  Created by Sasha Zats on 4/8/14.
+//  Copyright (c) 2014 Sasha Zats. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface ZTSAdjustFontSize : NSObject
+
+@end

8ff47b184b904c4b239b7537c138e731bdaa806b
Sasha Zats <sasha@zats.io>
2014-04-08 12:06:15 +0300

    Initial commit

--- AdjustFontSize/ZTSAdjustFontSize.m
+++ AdjustFontSize/ZTSAdjustFontSize.m
@@ -8,17 +8,31 @@
 
 #import "ZTSAdjustFontSize.h"
 
+static NSArray *ZTSNodeTypes;
+
+@interface DVTSourceNodeTypes : NSObject
++ (instancetype)nodeTypeNameForId:(NSString *)nodeId;
++ (NSInteger)nodeTypesCount;
+@end
+
+@interface DVTFontAndColorTheme : NSObject
++ (instancetype)currentTheme;
+- (void)setFont:(NSFont *)font forNodeTypes:(NSIndexSet *)nodeTypes;
+- (NSFont *)fontForNodeType:(DVTSourceNodeTypes *)nodeType;
+@end
+
+
 static ZTSAdjustFontSize *sharedPlugin;
 
 @interface ZTSAdjustFontSize()
-
 @property (nonatomic, strong) NSBundle *bundle;
 @end
 
 @implementation ZTSAdjustFontSize
 
-+ (void)pluginDidLoad:(NSBundle *)plugin
-{
+#pragma mark - Lifecycle
+
++ (void)pluginDidLoad:(NSBundle *)plugin {
     static id sharedPlugin = nil;
     static dispatch_once_t onceToken;
     NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
@@ -29,36 +43,108 @@ static ZTSAdjustFontSize *sharedPlugin;
     }
 }
 
-- (id)initWithBundle:(NSBundle *)plugin
-{
+- (id)initWithBundle:(NSBundle *)plugin {
     if (self = [super init]) {
-        // reference to plugin's bundle, for resource acccess
         self.bundle = plugin;
-        
-        // Create menu items, initialize UI, etc.
-
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
+        [self _setupNodeTypes];
+        [self _setupMenu];
     }
     return self;
 }
 
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
-    [alert runModal];
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+#pragma mark - Handlers
+
+- (void)_increaseFontSizeHandler {
+    DVTFontAndColorTheme *currentTheme = [self _currentTheme];
+    [self _enumerateFontsForTheme:currentTheme usingBlock:^(NSFont *font, NSString *nodeTypeID, DVTSourceNodeTypes *nodeType, BOOL *stop) {
+        NSFont *newFont = [NSFont fontWithDescriptor:font.fontDescriptor size:font.pointSize + 1];
+        [currentTheme setFont:newFont forNodeTypes:[NSIndexSet indexSetWithIndex:[ZTSNodeTypes indexOfObjectIdenticalTo:nodeTypeID]]];
+    }];
+}
+
+- (void)_decreaseFontSizeHandler {
+    DVTFontAndColorTheme *currentTheme = [self _currentTheme];
+    [self _enumerateFontsForTheme:currentTheme usingBlock:^(NSFont *font, NSString *nodeTypeID, DVTSourceNodeTypes *nodeType, BOOL *stop) {
+        NSFont *newFont = [NSFont fontWithDescriptor:font.fontDescriptor size:font.pointSize - 1];
+        [currentTheme setFont:newFont forNodeTypes:[NSIndexSet indexSetWithIndex:[ZTSNodeTypes indexOfObjectIdenticalTo:nodeTypeID]]];
+    }];
+}
+
+#pragma mark - Private
+
+- (void)_setupNodeTypes {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        ZTSNodeTypes = @[
+                         @"xcode.syntax.attribute",
+                         @"xcode.syntax.character",
+                         @"xcode.syntax.comment",
+                         @"xcode.syntax.comment.doc",
+                         @"xcode.syntax.comment.doc.keyword",
+                         @"xcode.syntax.identifier.class",
+                         @"xcode.syntax.identifier.class.system",
+                         @"xcode.syntax.identifier.constant",
+                         @"xcode.syntax.identifier.constant.system",
+                         @"xcode.syntax.identifier.function",
+                         @"xcode.syntax.identifier.function.system",
+                         @"xcode.syntax.identifier.macro",
+                         @"xcode.syntax.identifier.macro.system",
+                         @"xcode.syntax.identifier.type",
+                         @"xcode.syntax.identifier.type.system",
+                         @"xcode.syntax.identifier.variable",
+                         @"xcode.syntax.identifier.variable.system",
+                         @"xcode.syntax.keyword",
+                         @"xcode.syntax.number",
+                         @"xcode.syntax.plain",
+                         @"xcode.syntax.preprocessor",
+                         @"xcode.syntax.string",
+                         @"xcode.syntax.url",
+                         ];
+    });
+}
+
+- (void)_setupMenu {
+    NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"View"];
+    if (menuItem) {
+        [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+        
+        NSMenuItem *increaseFontSizeMenuItem = [[NSMenuItem alloc] initWithTitle:@"Increase font size"
+                                                                          action:@selector(_increaseFontSizeHandler)
+                                                                   keyEquivalent:@"+"];
+        increaseFontSizeMenuItem.target = self;
+        [[menuItem submenu] addItem:increaseFontSizeMenuItem];
+        
+        NSMenuItem *decreaseFontSizeMenuItem = [[NSMenuItem alloc] initWithTitle:@"Decrease font size"
+                                                                          action:@selector(_decreaseFontSizeHandler)
+                                                                   keyEquivalent:@"-"];
+        decreaseFontSizeMenuItem.target = self;
+        [[menuItem submenu] addItem:decreaseFontSizeMenuItem];
+    }
+}
+
+- (DVTFontAndColorTheme *)_currentTheme {
+    id fontAndColorThemeClass = NSClassFromString(@"DVTFontAndColorTheme");
+    DVTFontAndColorTheme *theme = [fontAndColorThemeClass currentTheme];
+    return theme;
+}
+
+- (void)_enumerateFontsForTheme:(DVTFontAndColorTheme *)theme usingBlock:(void (^)(NSFont *font, NSString *nodeTypeID, DVTSourceNodeTypes *nodeType, BOOL *stop))block {
+    id sourceNodeTypesClass = NSClassFromString(@"DVTSourceNodeTypes");
+    BOOL stop = NO;
+    for (NSString *nodeNameId in ZTSNodeTypes) {
+        DVTSourceNodeTypes *sourceNodeTypes = [sourceNodeTypesClass nodeTypeNameForId:nodeNameId];
+        NSFont *font = [theme fontForNodeType:sourceNodeTypes];
+        block(font, nodeNameId, sourceNodeTypes, &stop);
+        if (stop) {
+            break;
+        }
+    }
+    
 }
 
 @end
